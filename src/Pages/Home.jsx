b30f34b12bd19e9ee1f4052989a87fba474e09b7
nishantshukla0808 <136834729+nishantshukla0808@users.noreply.github.com>
2024-08-31 18:21:29 +0530

    Error Fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
-import Banner from '../components/Banner'
-import Banner2 from '../components/Banner2'
-import Footer from '../components/Footer'
+import {Navbar,Banner,Banner2,Footer} from "../components/compIndex"
 
 const Home = () => {
   return (
---
+++ src/components/compIndex.js
@@ -0,0 +1,6 @@
+import Navbar from "./navbar";
+import Banner from "./Banner";
+import Banner2 from "./Banner2";
+import Footer from "./Footer";
+
+export {Navbar,Banner,Banner2,Footer}
