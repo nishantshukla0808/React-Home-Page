808a2a00a3edf3b4c6e3fe2715cf89de64c8c76f
nishantshukla0808 <136834729+nishantshukla0808@users.noreply.github.com>
2024-08-31 18:26:51 +0530

    Update - navbar import statement

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
-import Footer from '../components/Footer'
+import {Navbar,Footer} from "../components/compIndex"
 
 const About = () => {
   return (
